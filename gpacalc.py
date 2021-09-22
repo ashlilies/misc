cbc8f6651e7f65301b3f74dc59e601fbe771b20c
Ashlee Tan <77949379+meowbie@users.noreply.github.com>
2021-09-22 12:16:14 +0800

    nyp gpa calc

---
+++ gpacalc.py
@@ -0,0 +1,52 @@
+# GPA Calculator for NYP
+# For Python 3.9
+# Created on: Sunday, 15 August 2021
+# Copyright (c) 2021 Ashlee Tan, qns: ash@meowbie.com
+
+# TODO: Add error handling, and maybe convert numgrd(s) to dict or fill up numgrd, and save to txt/csv automatically
+
+d = {}
+
+def numgrd(s):
+    if s == "A":
+        return 4.0
+    elif s == "B+":
+        return 3.5
+    elif s == "B":
+        return 3.0
+    elif s == "C+":
+        return 2.5
+    elif s == "C":
+        return 2.0
+    # TODO: add more grades, but this is all I got
+    else:
+        raise Exception
+
+print("Welcome to NYP GPA Calculator by Ashlee!")
+n = int(input("Please enter the number of graded subjects: "))
+for i in range(n):
+    name = input("Friendly Name (sub %d): " % (i+1))
+    grade = input("Alphabetical Grade (sub %d): " % (i+1)).upper()
+    credit = int(input("Credits (Hours per Week) (sub %d): " % (i+1)))
+    d[name] = {"grade": grade, "credit": credit}
+
+for k in d:
+    d[k]["ngrade"] = float(numgrd(d[k]["grade"]))
+
+creds = egps = 0
+for k in d:
+    egps += d[k]["ngrade"] * d[k]["credit"]
+    creds += d[k]["credit"]
+
+gpa = egps/creds
+
+print("=" * 14 + " YOUR GRADES " + "=" * 15)
+print("|| %s | %s | %s ||" % ("NAME".center(12), "GRADE".center(9), "HOURS".center(9)))
+print("|| " + "." * 36 + " ||")
+for k in d:
+    print("|| %s | %s | %s ||" % (k.center(12), str(str(d[k]["grade"]).center(2) + " (" + str(d[k]["ngrade"]) + ")").center(9), str(d[k]["credit"]).center(9)))
+print("=" * 42)
+
+print("-" * 12 + " YOUR OVERALL GPA " + "-" * 12)
+print("Overall GPA is: %1.3f" % gpa)
+print("Thanks for using this program!")
