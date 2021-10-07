fb01c27fbd76b10a7bba609c47881f0f4779c285
Ashlee Tan <ash@meowbie.com>
2021-10-07 17:12:18 +0800

    add cqueue

--- cqueue/cqueue.c
+++ cqueue/cqueue.c
@@ -1,5 +1,17 @@
 /* 
  * An implementation of a dynamically-sized FIFO queue
- * in C89 with linked lists.
+ * in C89 using linked lists.
+ * Update type accordingly as needed.
  * Copyright (C) 2021, Ashlee Tan. Licensed as attached.
  */
+
+#define   TYPE   int
+
+typedef struct elem {
+	TYPE content;
+	struct elem *next;
+} ELEM;
+
+ELEM *pq;  /* pointer to first element in queue */
+
+/* TODO */
