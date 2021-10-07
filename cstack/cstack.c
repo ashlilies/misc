b61a676f21a6a2022246ac913d5c8a5ccc0ddf05
Ashlee Tan <ash@meowbie.com>
2021-10-07 16:23:21 +0800

    Added Cstack

---
+++ cstack/cstack.c
@@ -0,0 +1,65 @@
+/* 
+ * An implementation of a dynamic FIFO stack in K&R C.
+ * Functions included: push(a), pop().
+ * Set type accordingly.
+ * Copyright (C) 2021, Ashlee Tan. Licensed as attached.
+ */
+
+#define   TYPE   int   /* type of elem inside the stack */
+
+#include <stdlib.h>    /* used by malloc() and friends */
+
+static TYPE *stack;    /* pointer to first element in stack */
+static int  pos;       /* next avail position of top of stack; indexed */
+static int  ssz;       /* current size of stack */
+
+push(a)  /* return 0 on success, 1 on failure */
+	TYPE a;
+{
+	TYPE *temp;  /* checking realloc failures */
+
+	if (!ssz) { /* not yet initialized; first call */
+		ssz = sizeof(TYPE);
+		stack = (TYPE *) malloc(ssz);
+		if (!stack)
+			return(1);  /* not enough memory */
+	}
+
+	if ((pos+1) * sizeof(TYPE) > ssz) { /* need more space */
+		temp = (TYPE *) realloc(stack, ssz += sizeof(TYPE));
+		if (!temp) {
+			ssz -= sizeof(TYPE);  /* undo */
+			return(1);  /* not enough memory */
+		}
+		stack = temp;
+	}
+
+	stack[pos++] = a;
+	return(0);
+}
+
+TYPE
+pop()  /* returns popped value or 0 if not found/error */
+{
+	TYPE val;
+	TYPE *temp;  /* realloc error checking */
+
+	if (!ssz)  /* stack doesn't exist yet */
+		return(0);
+
+	if (ssz && pos-1 < 0) {  /* the stack is now empty, but exists */
+		stack[0] = 0;    /* and tried to pop last element */
+		return(0);
+	}
+
+	val = stack[--pos];  /* get last stored val off stack */
+	if (ssz-sizeof(TYPE) >= sizeof(TYPE)) { /* make stack smaller */
+		temp = (TYPE *) realloc(stack, ssz -= sizeof(TYPE));
+		if (!temp) { /* an error occured */
+			ssz += sizeof(TYPE); /* undo */
+			return(0);
+		}
+	}
+
+	return(val);
+}
---
+++ cstack/test.c
@@ -0,0 +1,59 @@
+/*
+ * Test driver for cstack (K&R).
+ * Copyright (C) 2021, Ashlee Tan. Licensed as attached.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cstack.c"
+
+main()
+{
+	int  n;      /* value to be pushed */
+	int  count;
+	char *temp;  /* temporary input holding */
+
+	temp = malloc(sizeof(char) * 30); /* input buffer, size 30 */
+	printf("(Type ? for help)\n");
+	while (1) {
+		temp[0] = '\0';
+		printf("command: ");
+		fgets(temp, 29, stdin);
+		temp[strcspn(temp, "\n")] = '\0'; /* removes trailing '\n' */
+		
+		switch (temp[0]) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			n = atoi(temp);
+			printf("pushing %d\n", n);
+			push(n);
+			n = 0;   /* reset for next round */
+			break;
+		case 'q':
+			printf("exiting\n");
+			exit(0);
+		case 'p':  /* pop */
+			printf("popped %d\n", pop());
+			break;
+		case '?':
+			printf("Enter a number to push it to the stack, "
+			       "or enter a command.\n");
+			printf("Valid commands:\n"
+			       "\tq - quit the program\n"
+			       "\tp - pop last value off stack\n");
+			break;
+		default:
+			printf("invalid action %s\n", temp);
+			break;
+		}
+	}
+}
