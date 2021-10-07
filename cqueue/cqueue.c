cca837139b829553c12cb321b085e4cb025c770d
Ashlee Tan <ash@meowbie.com>
2021-10-07 17:22:20 +0800

    worked on cqueue

--- cqueue/cqueue.c
+++ cqueue/cqueue.c
@@ -1,11 +1,16 @@
 /* 
  * An implementation of a dynamically-sized FIFO queue
  * in C89 using linked lists.
+ * Functions included: enq(a), deq().
  * Update type accordingly as needed.
  * Copyright (C) 2021, Ashlee Tan. Licensed as attached.
  */
 
 #define   TYPE   int
+
+/* Function prototypes */
+int  enq(TYPE);
+TYPE deq(void);
 
 typedef struct elem {
 	TYPE content;
@@ -14,4 +19,20 @@ typedef struct elem {
 
 ELEM *pq;  /* pointer to first element in queue */
 
+/*
+ * Enqueues item a. Create a new queue if it doesn't exist.
+ * Returns 0 upon success, else return 1 for failure.
+ */
+int
+enq(TYPE a)
+{}
+
+/*
+ * Dequeues first element from queue.
+ * If exists, returns content in first element.
+ * Else if doesn't exist, return 0.
+ */
+TYPE
+deq(void)
+{}
 /* TODO */
